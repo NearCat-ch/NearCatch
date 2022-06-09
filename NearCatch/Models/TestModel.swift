0ac469fb7b20015238d3e18125d87e71338bd350
Wonhyuk Choi <whchoi000@gmail.com>
2022-06-09 15:52:46 +0900

    fix: 깃허브 폴더 빈파일 오류 수정

---
+++ NearCatch/Models/TestModel.swift
@@ -0,0 +1,8 @@
+//
+//  TestModel.swift
+//  NearCatch
+//
+//  Created by Wonhyuk Choi on 2022/06/09.
+//
+
+import Foundation
---
+++ NearCatch/Views/Shared/SharedTestView.swift
@@ -0,0 +1,20 @@
+//
+//  SharedTestView.swift
+//  NearCatch
+//
+//  Created by Wonhyuk Choi on 2022/06/09.
+//
+
+import SwiftUI
+
+struct SharedTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SharedTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        SharedTestView()
+    }
+}
