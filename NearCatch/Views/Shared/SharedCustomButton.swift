ffa9bf9da2f98f509e9c2a370c26b64ea176979e
Gold-MineScore <96890040+DevMizeKR@users.noreply.github.com>
2022-06-10 17:31:09 +0900

    feat: ProfileView 버튼 추가
    Profile View에 뒤로가기, 프로필 수정, 관심사 수정, 설정 버튼 추가

--- NearCatch/Views/Shared/SharedCustomButton.swift
+++ NearCatch/Views/Shared/SharedCustomButton.swift
@@ -11,6 +11,7 @@ struct SharedCustomButton: View {
     @State var icon:String
     @State var circleSize:CGFloat
     @State var color:Color
+    @State var innerOpacity:Double
     var body: some View {
         ZStack{
             ZStack{
@@ -20,9 +21,11 @@ struct SharedCustomButton: View {
                     .frame(width:circleSize * 1.2, height:circleSize*1.2)
                 Circle()
                     .fill(color)
-                    .opacity(0.5)
+                    .opacity(innerOpacity)
                     .frame(width:circleSize, height:circleSize)
                 Image(icon)
+                    .resizable()
+                    .frame(width:circleSize*0.5, height:circleSize*0.5)
             }
         }
     }
@@ -30,6 +33,6 @@ struct SharedCustomButton: View {
 
 struct SharedCustomButton_Previews: PreviewProvider {
     static var previews: some View {
-        SharedCustomButton(icon: "icn_edit", circleSize: 50, color:Color.white)
+        SharedCustomButton(icon: "icn_edit", circleSize: 50, color:Color.white, innerOpacity:0.5)
     }
 }
