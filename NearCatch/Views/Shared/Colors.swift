1139ef096fd732b04430a1746653cf57045c9fee
이준영 <74142881+User-Lawn@users.noreply.github.com>
2022-06-10 17:35:47 +0900

    feat : Colors

---
+++ NearCatch/Views/Shared/Colors.swift
@@ -0,0 +1,15 @@
+//
+//  Colors.swift
+//  NearCatch
+//
+//  Created by Jun.Mac on 2022/06/10.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let PrimaryColor = Color("PrimaryColor")
+    static let SecondaryColor = Color("Secondary")
+    static let ThirdColor = Color("ThirdColor")
+}
