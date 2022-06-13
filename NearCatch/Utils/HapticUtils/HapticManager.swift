e650c123c1c8c58d4c1091b814f17f76514ff529
era_i_ser <eraser3031@gmail.com>
2022-06-13 14:55:44 +0900

    feat: 햅틱 매니저 + 햅틱 테스트 뷰 수정

--- NearCatch/Utils/HapticUtils/HapticManager.swift
+++ NearCatch/Utils/HapticUtils/HapticManager.swift
@@ -14,22 +14,18 @@ class HapticManager: ObservableObject {
     private var engine: CHHapticEngine!
     private var advancedPlayer: CHHapticAdvancedPatternPlayer!
     
-    private let initialIntensity: Float = 1.0
-    private let initialSharpness: Float = 0.5
-    
     init() {
         self.createAndStartHapticEngine()
         self.initializeHaptic()
     }
     
-    func createAndStartHapticEngine() {
+    private func createAndStartHapticEngine() {
         do {
             engine = try CHHapticEngine()
         } catch let error {
             fatalError("Engine Creation Error: \(error)")
         }
         
-        // Mute audio to reduce latency for collision haptics.
         engine.playsHapticsOnly = true
         
         // The stopped handler alerts you of engine stoppage.
@@ -55,84 +51,36 @@ class HapticManager: ObservableObject {
             }
         }
         
-        // The reset handler provides an opportunity to restart the engine.
         engine.resetHandler = {
-            
             print("Reset Handler: Restarting the engine.")
-            
             do {
-                // Try restarting the engine.
                 try self.engine.start()
-                
-                // Recreate the continuous player.
                 self.initializeHaptic()
-                
             } catch {
                 print("Failed to start the engine")
             }
         }
-        
-        // Start the haptic engine for the first time.
-        do {
-            try self.engine.start()
-        } catch {
-            print("Failed to start the engine: \(error)")
-        }
     }
     
-    func updateHaptic(dist: Float?, matchingCount: Int) {
-        guard let dist = dist else { return }
-        let intensityValue = linearInterpolation(alpha: dist, min: 0, max: 10)
-//        advancedPlayer.playbackRate = intensityValue
-        let intensityParameter = CHHapticDynamicParameter(parameterID: .hapticIntensityControl,
-                                                          value: intensityValue,
-                                                          relativeTime: 0)
-        
-        do {
-            try advancedPlayer.sendParameters([intensityParameter], atTime: 0)
-        } catch let error {
-            print("Dynamic Parameter Error: \(error)")
-        }
-        
-    }
-    
-    private func linearInterpolation(alpha: Float, min: Float, max: Float) -> Float {
-        return alpha / (max - min)
-    }
-    
-    func initializeHaptic() {
+    private func initializeHaptic() {
         let pattern = createPatternFromAHAP("NearCatchHeartbeat")!
         
         do {
-            advancedPlayer = try? engine.makeAdvancedPlayer(with: pattern)
+            advancedPlayer = try engine.makeAdvancedPlayer(with: pattern)
             advancedPlayer?.loopEnabled = true
             advancedPlayer.playbackRate = 1
-            startPlayer(advancedPlayer)
         } catch {
-            print("erro...")
+            print("error...")
         }
     }
     
-    private func createPatternFromAHAP(_ filename: String) -> CHHapticPattern? {
-        // Get the URL for the pattern in the app bundle.
-        let patternURL = Bundle.main.url(forResource: filename, withExtension: "ahap")!
-        
+    func startHaptic() {
         do {
-//            try engine.playPattern(from: patternURL)
-            // Read JSON data from the URL.
-            let patternJSONData = try Data(contentsOf: patternURL, options: [])
-            
-            // Create a dictionary from the JSON data.
-            let dict = try JSONSerialization.jsonObject(with: patternJSONData, options: [])
-            
-            if let patternDict = dict as? [CHHapticPattern.Key: Any] {
-                // Create a pattern from the dictionary.
-                return try CHHapticPattern(dictionary: patternDict)
-            }
-        } catch let error {
-            print("Error creating haptic pattern: \(error)")
+            try engine.start()
+            startPlayer(advancedPlayer)
+        } catch {
+            print("asdfasdf")
         }
-        return nil
     }
     
     func endHaptic() {
@@ -143,10 +91,44 @@ class HapticManager: ObservableObject {
         }
     }
     
-    func startPlayer(_ player: CHHapticPatternPlayer) {
-//        guard supportsHaptics else { return }
+    func updateHaptic(dist: Float?, matchingPercent: Float) {
+        guard let dist = dist else { return }
+        let intensityValue = linearInterpolation(alpha: dist, min: 0, max: 10)
+        advancedPlayer.playbackRate = 1 + matchingPercent
+        let intensityParameter = CHHapticDynamicParameter(parameterID: .hapticIntensityControl,
+                                                          value: intensityValue,
+                                                          relativeTime: 0)
+        
         do {
-//            try startHapticEngineIfNecessary()
+            try advancedPlayer.sendParameters([intensityParameter], atTime: 0)
+        } catch let error {
+            print("Dynamic Parameter Error: \(error)")
+        }
+    }
+    
+    private func linearInterpolation(alpha: Float, min: Float, max: Float) -> Float {
+        return alpha / (max - min)
+    }
+    
+    private func createPatternFromAHAP(_ filename: String) -> CHHapticPattern? {
+        let patternURL = Bundle.main.url(forResource: filename, withExtension: "ahap")!
+        
+        do {
+            let patternJSONData = try Data(contentsOf: patternURL, options: [])
+            
+            let dict = try JSONSerialization.jsonObject(with: patternJSONData, options: [])
+            
+            if let patternDict = dict as? [CHHapticPattern.Key: Any] {
+                return try CHHapticPattern(dictionary: patternDict)
+            }
+        } catch let error {
+            print("Error creating haptic pattern: \(error)")
+        }
+        return nil
+    }
+    
+    private func startPlayer(_ player: CHHapticPatternPlayer) {
+        do {
             print("start")
             try player.start(atTime: CHHapticTimeImmediate)
         } catch let error {
@@ -154,11 +136,8 @@ class HapticManager: ObservableObject {
         }
     }
     
-    func stopPlayer(_ player: CHHapticPatternPlayer) {
-//        guard supportsHaptics else { return }
-        
+    private func stopPlayer(_ player: CHHapticPatternPlayer) {
         do {
-//            try startHapticEngineIfNecessary()
             try player.stop(atTime: CHHapticTimeImmediate)
         } catch let error {
             print("Error stopping haptic player: \(error)")
--- NearCatch/Utils/HapticUtils/HapticTestView.swift
+++ NearCatch/Utils/HapticUtils/HapticTestView.swift
@@ -9,27 +9,63 @@ import SwiftUI
 
 struct HapticTestView: View {
     
-    @StateObject var niManager = NISessionManager()
     @StateObject var hapticManager = HapticManager()
     
     // 테스트용
-    @State private var distance: Float = 0
-    @State private var matchingCount = 0
+    let timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
+    @State private var distance: Float = 10
+    @State private var matchingPercent: Float = 0
     
     var body: some View {
         VStack(alignment: .leading) {
+            Text("MatchingCount")
+            Slider(value: $matchingPercent, in: 0...1)
+            
+            Spacer()
+                .frame(height: 30)
+            
             Text("Distance")
             Slider(value: $distance, in: 0...10)
+            
+            HStack {
+                Button(role: .cancel) {
+                    // 햅틱 종료
+                    hapticManager.endHaptic()
+                } label: {
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12, style: .continuous)
+                            .fill(.gray)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 50)
+                        
+                        Text("멈춤")
+                            .foregroundColor(.white)
+                    }
+                }
+                
+                Button(role: .destructive) {
+                    // 햅틱 시작
+                    hapticManager.startHaptic()
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12, style: .continuous)
+                            .fill(.blue)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 50)
+                        
+                        Text("시작")
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+            .padding(.top, 40)
+
         }
         .padding(.horizontal, 30)
-         //실제 사용시
-//        .onChange(of: niManager.latestNearbyObject) { object in
-//            // matchingCount 프로퍼티 공통관심사 로직 확정되면 수정
-//            hapticManager.updateHaptic(dist: object?.distance, matchingCount: 1)
-//        }
-        // 테스트용
-        .onChange(of: distance) { dist in
-            hapticManager.updateHaptic(dist: distance, matchingCount: matchingCount)
+        .onReceive(timer) { _ in
+            // 거리 + 일치한 관심사 개수에 따라 햅틱 업데이트
+            hapticManager.updateHaptic(dist: distance, matchingPercent: matchingPercent)
         }
     }
 }
