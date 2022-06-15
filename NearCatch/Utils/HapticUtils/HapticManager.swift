f0d7b14b43b8807c47b472a37356df31594d48f4
eraser <eraser3031@gmail.com>
2022-06-15 21:38:28 +0900

    comment: Haptic 파일 주석 추가

--- NearCatch/Utils/HapticUtils/HapticManager.swift
+++ NearCatch/Utils/HapticUtils/HapticManager.swift
@@ -12,6 +12,7 @@ import CoreHaptics
 class HapticManager {
     
     private var engine: CHHapticEngine!
+    // 'Advanced' PatternPlayer만 재생 컨트롤(멈춤, 반복재생, 뒤로가기 ...) 등이 가능함
     private var advancedPlayer: CHHapticAdvancedPatternPlayer!
     
     init() {
@@ -20,6 +21,7 @@ class HapticManager {
         self.startHaptic()
     }
     
+    // 햅틱 엔진 생성
     private func createAndStartHapticEngine() {
         do {
             engine = try CHHapticEngine()
@@ -27,9 +29,10 @@ class HapticManager {
             fatalError("Engine Creation Error: \(error)")
         }
         
+        // 햅틱 엔진 오직 햅틱만 ( 원래는 소리도 같이 넣는 게 정석 )
         engine.playsHapticsOnly = true
         
-        // The stopped handler alerts you of engine stoppage.
+        // 엔진이 멈췄을 때 실행할 핸들러 지정
         engine.stoppedHandler = { reason in
             print("Stop Handler: The engine stopped for reason: \(reason.rawValue)")
             switch reason {
@@ -52,6 +55,7 @@ class HapticManager {
             }
         }
         
+        // 엔진이 리셋되었을 대 실행할 핸들러 지정
         engine.resetHandler = {
             print("Reset Handler: Restarting the engine.")
             do {
@@ -63,6 +67,7 @@ class HapticManager {
         }
     }
     
+    // 햅틱 초기화하기
     private func initializeHaptic() {
         let pattern = createPatternFromAHAP("NearCatchHeartbeat")!
         
@@ -75,10 +80,13 @@ class HapticManager {
         }
     }
     
+    // 햅틱 시작
     func startHaptic() {
         do {
             try engine.start()
             startPlayer(advancedPlayer)
+            
+            // 햅틱을 미리 시작해두고 NISession이 생성될 때 반응하기 위해 일단 intensity를 0으로 두어 햅틱이 안느껴지도록 함
             let intensityParameter = CHHapticDynamicParameter(parameterID: .hapticIntensityControl,
                                                               value: 0,
                                                               relativeTime: 0)
@@ -88,6 +96,7 @@ class HapticManager {
         }
     }
     
+    // 햅틱 종료
     func endHaptic() {
         do {
             try advancedPlayer.stop(atTime: CHHapticTimeImmediate)
@@ -95,9 +104,13 @@ class HapticManager {
             print("Error stopping the continuous haptic player: \(error)")
         }
     }
+    
+    // 진동 속도, 세기 기준은 일단 임의로 지정함.
     func updateHaptic(dist: Float?, matchingPercent: Int) {
         guard let dist = dist else { return }
+        // 최대 9mm 상정하고 0~1사이의 값 만든 후 hapticIntensityControl의 최대값인 1에서 빼주는 식으로 진동 세기 구현
         let intensityValue = dist / 9
+        // 배속이 최대 3배속까지 안어색한 거같아서 매칭 최대 개수 10개시 3배속이 되도록 진동 속도 구현
         advancedPlayer.playbackRate = 1 + Float(matchingPercent) / 5
         let intensityParameter = CHHapticDynamicParameter(parameterID: .hapticIntensityControl,
                                                           value: 1 - intensityValue,
@@ -109,11 +122,9 @@ class HapticManager {
             print("Dynamic Parameter Error: \(error)")
         }
     }
-
-    private func linearInterpolation(alpha: Float, min: Float, max: Float) -> Float {
-        return alpha / (max - min)
-    }
     
+    // AHAP( Apple Haptic and Audio Pattern ) 햅틱을 JSON 형식으로 표현할 파일 -> 심장소리
+    // 해당 AHAP 파일을 불러옴
     private func createPatternFromAHAP(_ filename: String) -> CHHapticPattern? {
         let patternURL = Bundle.main.url(forResource: filename, withExtension: "ahap")!
         
@@ -131,6 +142,7 @@ class HapticManager {
         return nil
     }
     
+    // HapticPattern을 틀어줄 Player 객체 생성 및 플레이
     private func startPlayer(_ player: CHHapticPatternPlayer) {
         do {
             print("start")
