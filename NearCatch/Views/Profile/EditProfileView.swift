062bcce45b2281a9dc4deb8d1e569327709e7d69
Gold-MineScore <96890040+DevMizeKR@users.noreply.github.com>
2022-06-16 15:25:33 +0900

    feat: KeywordChangeView에 키워드 수정 기능 추가

--- NearCatch/Views/Profile/EditProfileView.swift
+++ NearCatch/Views/Profile/EditProfileView.swift
@@ -67,7 +67,6 @@ struct EditProfileView: View {
                                     }
                                 }
                             }
-                            
                         }
                         Rectangle()
                             .frame(width:260, height: 1)
@@ -82,7 +81,7 @@ struct EditProfileView: View {
                     Button{
                         action: do { self.presentationMode.wrappedValue.dismiss() }
                     } label:{
-                        SharedRectangularButton(rectWidth:350, rectColor:((nickname.isEmpty || nickname.count > 10) ? Color.gray : Color.PrimaryColor), text:"수정하기", textColor:((nickname.isEmpty || nickname.count > 10) ? Color.white : Color.black))
+                        SharedRectangularButton(rectWidth:350, rectColor:((nickname.isEmpty || nickname.count > 10) ? Color.ThirdColor : Color.PrimaryColor), text:"수정하기", textColor:((nickname.isEmpty || nickname.count > 10) ? Color.white : Color.black))
                     }.disabled(nickname.isEmpty || nickname.count > 10)
                     Spacer()
                 }
--- NearCatch/Views/Profile/KeywordChangeView.swift
+++ NearCatch/Views/Profile/KeywordChangeView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct KeywordChangeView: View {
+    @StateObject var tagData = TagViewModel()
+    @ObservedObject var togglecount = ToggleCount()
+    @State var tag:Int? = nil
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     var body: some View {
         ZStack {
             Image("img_background")
                 .edgesIgnoringSafeArea([.top])
             VStack{
-                Spacer()
-                    .frame(height:60)
                 Button(
                     action: {
                         presentationMode.wrappedValue.dismiss()
@@ -25,7 +26,7 @@ struct KeywordChangeView: View {
                         .frame(width:50, height:5)
                 }
                 Spacer()
-                    .frame(height:70)
+                    .frame(height:60)
                 HStack {
                     Text("관심사")
                         .font(.custom("온글잎 의연체", size:34))
@@ -34,13 +35,181 @@ struct KeywordChangeView: View {
                         .font(.custom("온글잎 의연체", size:34))
                         .foregroundColor(.white)
                 }
-                Text("최소 5개 이상 선택하셔야합니다!")
-                    .font(.custom("온글잎 의연체", size:22))
-                    .foregroundColor(.white)
-                Text("5 / 10")
-                    .font(.custom("온글잎 의연체", size:34))
-                    .foregroundColor(.white)
+                Text("최소 5개 이상 선택해야 해요!")
+                    .font(.custom("온글잎 의연체", size: 22))
+                    .foregroundColor((togglecount.keywordCounter < 5) ? Color.red : Color.white)
+                Text("\(togglecount.keywordCounter) / 10")
+                    .font(.custom("온글잎 의연체", size: 34))
+                    .foregroundColor((togglecount.keywordCounter < 5 || togglecount.keywordCounter > 10) ? Color.red : Color.white)
                 Spacer()
+                    .frame(height:100)
+                
+                ScrollView(.horizontal) {
+                    VStack(alignment: .leading){
+                        HStack{
+                            ForEach(0..<14, id: \.self) { i in
+                                    Button(action: {
+                                        if tagData.Tags[i].isSelected == true {
+                                            tagData.Tags[i].isSelected = false
+                                            self.togglecount.keywordCounter -= 1
+                                        } else {
+                                            tagData.Tags[i].isSelected = true
+                                            self.togglecount.keywordCounter += 1
+                                        }
+                                    }){
+                                        ZStack{
+                                            if tagData.Tags[i].isSelected == true{
+                                                Text(tagData.Tags[i].name)
+                                                    .tagView(.black, .PrimaryColor)
+                                            }
+                                            else{
+                                                Text(tagData.Tags[i].name)
+                                                    .tagView(.gray, .ThirdColor)
+                                            }
+                                        }
+                                    }
+                            }
+                        }
+                        HStack{
+                            ForEach(14..<30, id: \.self) { i in
+                                Button(action: {
+                                    if tagData.Tags[i].isSelected == true {
+                                        tagData.Tags[i].isSelected = false
+                                        self.togglecount.keywordCounter -= 1
+                                    } else {
+                                        tagData.Tags[i].isSelected = true
+                                        self.togglecount.keywordCounter += 1
+                                    }
+                                }){
+                                    ZStack{
+                                        if tagData.Tags[i].isSelected == true{
+                                            Text(tagData.Tags[i].name)
+                                                .tagView(.black, .PrimaryColor)
+                                        }
+                                        else{
+                                            Text(tagData.Tags[i].name)
+                                                .tagView(.gray, .ThirdColor)
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        HStack{
+                            ForEach(30..<45, id: \.self) { i in
+                                Button(action: {
+                                    
+                                    if tagData.Tags[i].isSelected == true {
+                                        tagData.Tags[i].isSelected = false
+                                        self.togglecount.keywordCounter -= 1
+                                    } else {
+                                        tagData.Tags[i].isSelected = true
+                                        self.togglecount.keywordCounter += 1
+                                    }
+                                }){
+                                    ZStack{
+                                        if tagData.Tags[i].isSelected == true{
+                                            Text(tagData.Tags[i].name)
+                                                .tagView(.black, .PrimaryColor)
+                                        }
+                                        else{
+                                            Text(tagData.Tags[i].name)
+                                                .tagView(.gray, .ThirdColor)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        HStack{
+                            ForEach(45..<60, id: \.self) { i in
+                                Button(action: {
+                                    if tagData.Tags[i].isSelected == true {
+                                        tagData.Tags[i].isSelected = false
+                                        self.togglecount.keywordCounter -= 1
+                                    } else {
+                                        tagData.Tags[i].isSelected = true
+                                        self.togglecount.keywordCounter += 1
+                                    }
+                                }){
+                                    if tagData.Tags[i].isSelected == true{
+                                        Text(tagData.Tags[i].name)
+                                            .tagView(.black, .PrimaryColor)
+                                    }
+                                    else{
+                                        Text(tagData.Tags[i].name)
+                                            .tagView(.gray, .ThirdColor)
+                                    }
+                                }
+                                
+                            }
+                        }
+                        HStack{
+                            ForEach(60..<75, id: \.self) { i in
+                                Button(action: {
+                                    
+                                    if tagData.Tags[i].isSelected == true {
+                                        tagData.Tags[i].isSelected = false
+                                        self.togglecount.keywordCounter -= 1
+                                    } else {
+                                        tagData.Tags[i].isSelected = true
+                                        self.togglecount.keywordCounter += 1
+                                    }
+                                }){
+                                    ZStack{
+                                        if tagData.Tags[i].isSelected == true{
+                                            Text(tagData.Tags[i].name)
+                                                .tagView(.black, .PrimaryColor)
+                                        }
+                                        else{
+                                            Text(tagData.Tags[i].name)
+                                                .tagView(.gray, .ThirdColor)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        HStack{
+                            ForEach(75..<86, id: \.self) { i in
+                                Button(action: {
+                                    
+                                    if tagData.Tags[i].isSelected == true {
+                                        tagData.Tags[i].isSelected = false
+                                        self.togglecount.keywordCounter -= 1
+                                    } else {
+                                        tagData.Tags[i].isSelected = true
+                                        self.togglecount.keywordCounter += 1
+                                    }
+                                }){
+                                    if tagData.Tags[i].isSelected == true{
+                                        Text(tagData.Tags[i].name)
+                                            .tagView(.black, .PrimaryColor)
+                                    }
+                                    else{
+                                        Text(tagData.Tags[i].name)
+                                            .tagView(.gray, .ThirdColor)
+                                    }
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+                Spacer()
+                    .frame(height:140)
+                HStack{
+                    Button{
+                        action: do { self.presentationMode.wrappedValue.dismiss() }
+                    } label:{
+                        SharedRectangularButton(rectWidth:150, rectColor:.ThirdColor, text:"취소", textColor:.white)
+                    }
+                    Spacer()
+                        .frame(width:20)
+                    Button{
+                        action: do { self.presentationMode.wrappedValue.dismiss() }
+                    } label:{
+                        SharedRectangularButton(rectWidth:150, rectColor: (togglecount.keywordCounter < 5 || togglecount.keywordCounter > 10) ? .ThirdColor : .PrimaryColor, text:"수정", textColor:(togglecount.keywordCounter < 5 || togglecount.keywordCounter > 10) ? .white : .black)
+                    }.disabled(togglecount.keywordCounter < 5 || togglecount.keywordCounter > 10)
+                }
             }
         }
     }
