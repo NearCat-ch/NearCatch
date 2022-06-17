8b736facb5c5155f0a4a611c418c9ad1e66ec982
Wonhyuk Choi <whchoi000@gmail.com>
2022-06-18 02:27:29 +0900

    feat: 앱 클립을 기본 세팅 추가

--- NearCatch/Models/TestModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TestModel.swift
-//  NearCatch
-//
-//  Created by Wonhyuk Choi on 2022/06/09.
-//
-
-import Foundation
---
+++ NearCatchClip/NearCatchClipApp.swift
@@ -0,0 +1,18 @@
+//
+//  NearCatchClipApp.swift
+//  NearCatchClip
+//
+//  Created by Wonhyuk Choi on 2022/06/18.
+//
+
+import SwiftUI
+
+@main
+struct NearCatchClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
